9d8ef6b67bf805d3a079af239218c8fe082d4064
SneakBug8 <sneakbug8@gmail.com>
2021-04-22 20:09:03 +0300

    improved province changer

--- warcraft/history/provinces/province_changer.py
+++ warcraft/history/provinces/province_changer.py
@@ -1,8 +1,8 @@
 import os
 import re
 
-while True:
-    target_id = input("Print province id to reduce dev")
+def reduce_dev():
+    target_id = input("Print province id to reduce dev:\n")
     files = os.listdir(".")
 
     i = 0
@@ -30,3 +30,66 @@ while True:
             i += 1
             fh = None
     print("Wrote " + str(i) + " files")
+
+def find_provinces():
+    target= input("\nPrint regexp to find:\n")
+    files = os.listdir(".")
+
+    for f in files:
+        fh = open(f, "r")
+        ft = fh.read()
+
+        provid = re.search("[0-9]+", f)
+
+        if re.search(target, ft):
+            print(provid.group(), end = " ")
+
+        fh = None
+
+def remove_from_string():
+    replace_from = input("Replace from:\n")
+    replace_what = input("Replace what:\n").split(" ")
+
+    for f in replace_what:
+        replace_from = replace_from.replace(f, "")
+
+    replace_from = replace_from.replace("  ", " ")
+    print()
+    print()
+    print(replace_from)
+
+def append_to_provinces():
+    target= input("\nPrint regexp to find:\n")
+    files = os.listdir(".")
+
+    i = 0
+    for f in files:
+        fh = open(f, "r")
+        ft = fh.read()
+
+        # provid = re.search("[0-9]+", f)
+
+        if re.search(target, ft) or re.search(target, f):
+            fh = open(f, "w")
+
+            ft += """\n# Syndicate append
+595.1.1 = {
+owner = SYD
+controller = SYD
+add_core = SYD
+}"""
+            fh.write(ft)
+            i += 1
+        fh = None
+    print("Wrote " + str(i) + " files")
+
+while True:
+    mode = input("Select desired mode:\n")
+    if mode == "1":
+        reduce_dev()
+    elif mode == "2":
+        find_provinces()
+    elif mode == "3":
+        remove_from_string()
+    elif mode == "4":
+        append_to_provinces()
